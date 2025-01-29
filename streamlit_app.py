a244768d5280e576ae489c2024215df583f655b2
Khairunsa <khairunnisaauliarahma7785@gmail.com>
2025-01-29 14:18:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,4 +89,4 @@ df_prediction_proba.rename (columns ={ 0: 'Adelie',
 st.subheader('Predicted Species')
 df_prediction_proba
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
-st.success(str(penguin_species[prediction][0]))
+st.success(str(penguins_species[prediction][0]))
